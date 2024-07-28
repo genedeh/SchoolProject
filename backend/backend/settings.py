d7628898756758eab552a8a063a42e0f857c0ee4
genedeh <genesis.enedeh@gmail.com>
2024-07-28 23:40:11 +0100

    Link Backend Database To Mysql..

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,12 +74,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'oams',  
+        'USER': 'root',  
+        'PASSWORD': 'Genesis@05',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 
 # Password validation
