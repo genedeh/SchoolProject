fc49729d0bc3638cb21c5837df6503952d42afc8
genedeh <genesis.enedeh@gmail.com>
2024-07-29 15:15:08 +0100

    Added Result Model

--- backend/academics/models.py
+++ backend/academics/models.py
@@ -3,6 +3,7 @@ from user.models import User
 
 # Create your models here.
 
+
 class ClassRoom(models.Model):
     name = models.CharField(max_length=8, unique=True)
     no_of_students = models.IntegerField(verbose_name="Number Of Students")
@@ -17,3 +18,24 @@ class Subject(models.Model):
 
     def __str__(self) -> str:
         return f"${self.assigned_class.name}_${self.name}"
+    
+class Result(models.Model):
+    TERM_CHOICES = [
+        ('1st_term', '1ST TERM'),
+        ('2nd_term', '2ND TERM'),
+        ('3rd_term', '3RD TERM'),
+    ]
+
+    year_span = models.CharField(max_length=10, help_text="For Example: 2023/2024")
+    assigned_class = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    assigned_student = models.ForeignKey(User, on_delete=models.CASCADE)
+    term = models.CharField(max_length=10, choices=TERM_CHOICES, default='1st_term')
+    name = models.CharField(max_length=50, default=f"${assigned_student.username}_${year_span}_${term}_Result")
+    uploaded = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    
