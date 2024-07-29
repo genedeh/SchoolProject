c1551b1755d3b33e28afe3c0f249d28fea23ed7f
genedeh <genesis.enedeh@gmail.com>
2024-07-29 22:20:02 +0100

    Added ClassRoomAdmin to restrict Relationship Selecting

--- backend/academics/admin.py
+++ backend/academics/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import ClassRoom, Subject, Result, UploadList
+from user.models import User
 # Register your models here.
 
-admin.site.register(ClassRoom)
+
+class ClassRoomAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "assigned_Teacher":
+            kwargs["queryset"] = User.objects.filter(is_student_or_teacher=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "students":
+            kwargs["queryset"] = User.objects.filter(is_student_or_teacher=True)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+admin.site.register(ClassRoom, ClassRoomAdmin)
 admin.site.register(Subject)
 admin.site.register(Result)
 admin.site.register(UploadList)
