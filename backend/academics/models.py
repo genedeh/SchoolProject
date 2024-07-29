09bbf94a0eecf45add7dbea1b522b2e3a2ddb5f0
genedeh <genesis.enedeh@gmail.com>
2024-07-29 14:48:44 +0100

    Added ClassRoom Model

--- backend/academics/models.py
+++ backend/academics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+
+class ClassRoom(models.Model):
+    class_name = models.CharField(max_length=8, unique=True)
+    no_of_students = models.IntegerField(verbose_name="Number Of Students")
+    assigned_Teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.class_name
