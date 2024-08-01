5e98ece0783c7bd6a4f7879dc7a9345a18e20842
genedeh <genesis.enedeh@gmail.com>
2024-08-01 22:57:42 +0100

    Created UserProfile Api View

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
-from .models import User
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 class LoginView(generics.GenericAPIView):
     serializer_class = UserSerializer
@@ -21,3 +24,16 @@ class LoginView(generics.GenericAPIView):
             })
         else:
             return Response({'error': 'Invalid Credentials'}, status=400)
+        
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        user = request.user
+        user_data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+        }
+        return Response(user_data)
