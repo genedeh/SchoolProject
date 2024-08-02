819ac5ed1147e69bace55c0c86963ca4bd07bcea
genedeh <genesis.enedeh@gmail.com>
2024-08-02 14:39:26 +0100

    Changed Removed Username input and Added FirstName and LastName input

--- frontend/src/components/Authentication/Login.components.jsx
+++ frontend/src/components/Authentication/Login.components.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
     const [username, setUsername] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
     let navigate = useNavigate();
+    useEffect(() => setUsername(`${firstname}_${lastname}`)
+        , [firstname, lastname])
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
@@ -17,16 +21,26 @@ const LoginForm = () => {
         } catch (err) {
             setMessage('Invalid Credentials');
         }
+
+
     };
     return (
         <div>
             <form onSubmit={handleLogin}>
                 <div>
-                    <label>Username:</label>
+                    <label>Firstname:</label>
                     <input
                         type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Lastname:</label>
+                    <input
+                        type="text"
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}
                     />
                 </div>
                 <div>
