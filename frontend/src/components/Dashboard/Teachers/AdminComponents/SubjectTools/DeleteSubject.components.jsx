e36ed475db04d0dd5efa6b0456b1d7dc6d043eeb
genedeh <genesis.enedeh@gmail.com>
2024-08-28 14:31:55 +0100

    Added a Seprate file for Delete Feature

---
+++ frontend/src/components/Dashboard/Teachers/AdminComponents/SubjectTools/DeleteSubject.components.jsx
@@ -0,0 +1,68 @@
+import { Modal, Button } from 'react-bootstrap';
+import { useState, useContext } from 'react';
+import { SubjectsContext } from '../../../../../contexts/Subjects.contexts';
+import axios from 'axios';
+
+export const DeleteSubjectModal = ({ show, handleClose, subjectId }) => {
+    const [deleteStatus, setDeleteStatus] = useState(null);
+    const { subjects, setSubjects } = useContext(SubjectsContext);
+
+    const handleDelete = async () => {
+        try {
+            const response = await axios.delete(`http://127.0.0.1:8000/api/subjects/${subjectId}/`);
+            console.log(response.data)
+            setDeleteStatus("success");
+            setSubjects(subjects.filter(subject => subject.id !== subjectId));
+        } catch (error) {
+            setDeleteStatus("failed");
+        }
+    };
+
+
+    return (
+        <>
+            <Modal show={show} onHide={() => {
+                handleClose()
+                setDeleteStatus(null);
+            }}>
+                {deleteStatus == null ?
+                    (
+                        <>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Delete Subject</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                Are you sure you want to delete this subject?
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Cancel
+                                </Button>
+                                <Button variant="danger" onClick={handleDelete}>
+                                    Delete
+                                </Button>
+                            </Modal.Footer>
+                        </>) :
+                    (<>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{deleteStatus === "success" ? "Success" : "Error"}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {deleteStatus === "success"
+                                ? "The subject was deleted successfully."
+                                : "Failed to delete the subject."}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={() => {
+                                handleClose()
+                                setDeleteStatus(null);
+                            }}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </>)}
+
+            </Modal>
+        </>
+    );
+};
