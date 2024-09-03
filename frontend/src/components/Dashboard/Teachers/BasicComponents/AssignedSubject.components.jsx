e6b9f0630733df5aa46eaa1fabd64c51cd9d07a4
genedeh <genesis.enedeh@gmail.com>
2024-09-03 08:49:54 +0100

    Fixed SubjectList Conflict with subjectsContext

--- frontend/src/components/Dashboard/Teachers/BasicComponents/AssignedSubject.components.jsx
+++ frontend/src/components/Dashboard/Teachers/BasicComponents/AssignedSubject.components.jsx
@@ -1,71 +1,57 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../../../contexts/User.contexts";
-import { UsersListContext } from "../../../../contexts/UsersList.contexts"
 import { SubjectsContext } from "../../../../contexts/Subjects.contexts"
 import { Card, Spinner, Button, ListGroup, Accordion } from 'react-bootstrap'
 import { GenderFemale, GenderMale } from "react-bootstrap-icons"
-import axios from "axios";
 
 export const AssignedSubjects = () => {
     const { currentUser } = useContext(UserContext);
-    const teacherId = currentUser["id"]
-    const [subjectList, setSubjectList] = useState([]);
     const { subjects } = useContext(SubjectsContext); // Access the list of users from context
 
-    // Fetch subjects assigned to a teacher
-    useEffect(() => {
-        const fetchSubjects = () => {
-            console.log(currentUser["teaching_subjects"])
-            console.log(subjects)
-            const subject = subjects.filter(subject => {
-                return currentUser["teaching_subjects"].includes(subject.id)
-            })
-            console.log(subject)
-            setSubjectList([subjectList, ...subject])
-        };
-        console.log(teacherId)
-        fetchSubjects();
-    }, []);
-
     if (!currentUser.is_student_or_teacher && currentUser && !currentUser.is_admin) {
-        if (subjectList.length !== 0) {
+        if (subjects.length !== 0) {
             return (
                 <div>
                     <center>
-                       <hr /> <h3>Assigned Subjects</h3><hr />
+                        <hr /> <h3>Assigned Subjects</h3><hr />
                     </center>
                     <Accordion flush={true} className="m-3">
-                        {subjects.map(({ id, name, students_offering }) => (
-                            <Card key={id} className="mb-2">
-                                <Accordion.Item eventKey={id}>
-                                    <Accordion.Header eventKey={id} className="d-flex justify-content-between align-items-center">
-                                        <div>
-                                            <h5>{name.replace('_', ' ')}</h5>
-                                        </div>
+                        {subjects.map((subject) => {
+                            const { id, name, students_offering } = subject;
+                            if (currentUser["teaching_subjects"].includes(id)) {
+                                return (
+                                    <Card key={id} className="mb-2">
+                                        <Accordion.Item eventKey={id}>
+                                            <Accordion.Header eventKey={id} className="d-flex justify-content-between align-items-center">
+                                                <div>
+                                                    <h5>{name.replace('_', ' ')}</h5>
+                                                </div>
 
-                                    </Accordion.Header>
-                                    <Accordion.Body>
-                                        <hr /><h5>Students</h5><hr />
-                                        <ListGroup>
-                                            {students_offering && students_offering.length !== 0 ? (students_offering.map(({ id, username, gender }) => (
-                                                <ListGroup.Item key={id}>
-                                                    {username.replace('_', ' ')}
-                                                    {gender === 'male' ?
-                                                        (<Button className="m-2" size="sm" variant='primary' style={{ 'borderColor': 'white' }}>
-                                                            <GenderMale />
-                                                        </Button>) :
-                                                        (<Button className="m-2" size="sm" style={{ 'backgroundColor': 'pink', 'borderColor': 'white' }}>
-                                                            <GenderFemale />
-                                                        </Button>)}
-                                                </ListGroup.Item>
-                                            ))) : ('NO Student Available')}
-                                        </ListGroup>
-                                    </Accordion.Body>
-                                </Accordion.Item>
+                                            </Accordion.Header>
+                                            <Accordion.Body>
+                                                <hr /><h5>Students</h5><hr />
+                                                <ListGroup>
+                                                    {students_offering && students_offering.length !== 0 ? (students_offering.map(({ id, username, gender }) => (
+                                                        <ListGroup.Item key={id}>
+                                                            {username.replace('_', ' ')}
+                                                            {gender === 'male' ?
+                                                                (<Button className="m-2" size="sm" variant='primary' style={{ 'borderColor': 'white' }}>
+                                                                    <GenderMale />
+                                                                </Button>) :
+                                                                (<Button className="m-2" size="sm" style={{ 'backgroundColor': 'pink', 'borderColor': 'white' }}>
+                                                                    <GenderFemale />
+                                                                </Button>)}
+                                                        </ListGroup.Item>
+                                                    ))) : ('NO Student Available')}
+                                                </ListGroup>
+                                            </Accordion.Body>
+                                        </Accordion.Item>
 
-                            </Card>
-                        ))}
+                                    </Card>
+                                )
+                            }
+                        })}
                     </Accordion>
                 </div>
             )
