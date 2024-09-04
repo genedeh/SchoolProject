6280b03d78feb64255113333cde1d3bc3475e990
genedeh <genesis.enedeh@gmail.com>
2024-09-04 21:42:21 +0100

    Changed Classroom and subject context useEffect dependencies

--- frontend/src/contexts/Subjects.contexts.jsx
+++ frontend/src/contexts/Subjects.contexts.jsx
@@ -18,13 +18,12 @@ export const SubjectsProvider = ({ children }) => {
                     const response = await axios.get('api/subjects/')
 
                     setSubjects(response.data)
-                    console.log(subjects)
                 } catch (err) {
                     console.log('There was an error fetching the items!', err)
                 }
             }
         }
         fetchSubjects();
-    }, [])
+    }, [subjects])
     return <SubjectsContext.Provider value={value}>{children}</SubjectsContext.Provider>
 }
