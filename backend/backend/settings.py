4f86df135b5117b2653e933d524120b1098ab6fd
genedeh <genesis.enedeh@gmail.com>
2024-10-29 13:00:03 +0100

    Changed database server to postgresql and hosting database on vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,12 +120,12 @@ SIMPLE_JWT = {
 
 DATABASES = {  
     'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': os.getenv("DB_NAME"),  
-        'USER': os.getenv("DB_USER"),  
-        'PASSWORD': os.getenv("DB_PWD"),  
-        'HOST': os.getenv("DB_HOST"),  
-        'PORT': os.getenv("DB_PORT"), 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DATABASE"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }  
 }  
 
