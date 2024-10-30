11a006004059836b9347949922d20ae903f5709f
genedeh <genesis.enedeh@gmail.com>
2024-10-30 15:32:48 +0100

    Changed Static File Dirs In settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,6 +169,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dpbfmlvpt',
     'API_KEY': '764877144671827',
@@ -176,9 +177,8 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
